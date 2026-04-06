7dc2eb2cc0c10375e8661581fac263fc3c0542bb
Cody Nguyen <cmnguyen2424@gmail.com>
2026-04-06 13:46:48 -0500

    Adjust TURN_ANGLE_DEG to 15.0 and set TURN_RATE_DEG_S to 45.0; increase sleep duration to 4.0 seconds for smoother navigation

--- UGV_Drive/Challenge2.py
+++ UGV_Drive/Challenge2.py
@@ -11,8 +11,8 @@ UGV_BAUD_RATE = 115200
 SPEED_MPH = 0.8
 INITIAL_DISTANCE_FT = 7     # x coordinate
 SECOND_DISTANCE_FT = 9.0    # y coordinate
-TURN_ANGLE_DEG = 35.0
-TURN_RATE_DEG_S = 15.0
+TURN_ANGLE_DEG = 15.0
+TURN_RATE_DEG_S = 45.0
 
 # Unit conversions
 FT_TO_M = 0.3048
@@ -265,7 +265,7 @@ def main():
         drive_distance(vehicle, INITIAL_DISTANCE_M, SPEED_MPS)
         time.sleep(1.0)
         turn_left(vehicle, TURN_ANGLE_DEG, TURN_RATE_DEG_S, flashing=False)
-        time.sleep(1.0)
+        time.sleep(4.0)
         drive_distance(vehicle, SECOND_DISTANCE_M, SPEED_MPS)
 
         print("Move complete. Disarming...")
