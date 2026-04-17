0308371f2995962c1db0e8967060a101a504c65f
Cody Nguyen <cmnguyen2424@gmail.com>
2026-04-16 22:58:41 -0500

    a

--- UGV_Drive/Challenge1.py
+++ UGV_Drive/Challenge1.py
@@ -9,7 +9,7 @@ UGV_BAUD_RATE = 115200
 
 # Mission parameters
 SPEED_MPH = 0.8
-INITIAL_DISTANCE_FT = 8.0     # first leg
+INITIAL_DISTANCE_FT = 60.0     # first leg
 SECOND_DISTANCE_FT = 0.0       # second leg not used in Challenge 1
 TURN_ANGLE_DEG = 75.0
 TURN_RATE_DEG_S = 10.0
